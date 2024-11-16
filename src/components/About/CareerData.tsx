29f67024c3aadcda87821a749d28885247d2444e
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-11-16 12:47:48 +0900

    fixed data

--- src/components/About/CareerData.tsx
+++ src/components/About/CareerData.tsx
@@ -26,13 +26,17 @@ export const CareerData: CareerDataItem[] = [
 
     },
     {
+        title: 'STIP2023, SIIT (Sirindhorn International Institute of Technology) Thammasat University, Thailand.',
+        date: '2024年4月',
+    },
+    {
         title: '高知県立大学法人 高知工科大学 情報学群 卒業予定',
-        date: '2024年3月',
+        date: '2025年3月',
 
     },
     {
         title: '総合研究大学院大学 情報学コース (国立情報学研究所) 入学予定',
-        date: '2024年4月',
+        date: '2025年4月',
     },
 ]
 
