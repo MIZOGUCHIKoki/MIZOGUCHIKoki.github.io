ad83486e06b677524b96ca93da1bf1ccefe1218c
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2025-07-08 16:06:00 +0900

    add paper

--- src/components/Research/PaperData.tsx
+++ src/components/Research/PaperData.tsx
@@ -3,7 +3,7 @@ import { PaperDataItem } from './PaperType';
 export const PaperData: PaperDataItem[] = [
     {
         title: 'IoT環境に適したワンタイムパスワード認証方式',
-        author: '溝口 洸熙, 清水 明宏',
+        author: '溝口 洸熙 (KUT), 清水 明宏 (KUT)',
         juranl: '電子情報通信学会 信学技報',
         vol: 124,
         no: 257,
@@ -13,9 +13,24 @@ export const PaperData: PaperDataItem[] = [
         type: 'Report',
         year: 2024,
         doi: '',
-        url: '',
+        url: 'https://ken.ieice.org/ken/paper/20241115Qc50/',
+    },
+    {
+        title: 'A PUF-based Lightweight Authentication and Key Exchange Protocol for IoT with Privacy Protection and Concealed CRP',
+        author: 'Koki Mizoguchi (SOKENDAI), Somchart Fugkeaw (SIIT), Akihiro Shimizu (KUT), Masahito Kumazaki (NII), Hirokazu Hasegawa (NII), Hiroki Takakura (NII)',
+        juranl: 'IEICE Technical Report',
+        vol: 125,
+        no: 99,
+        docs: 'ISEC2025-63',
+        page: '312-319',
+        date: '2025-06-30',
+        type: 'Report',
+        year: 2025,
+        doi: '',
+        url: 'https://ken.ieice.org/ken/paper/20250709NclD/',
     }
 ]
+
 
 /*
     title: string;
--- src/components/Research/PresentationData.tsx
+++ src/components/Research/PresentationData.tsx
@@ -3,11 +3,21 @@ import { PresentationDataItem } from './PaperType';
 export const PresentationData: PresentationDataItem[] = [
     {
         title: 'IoT環境に適したワンタイムパスワード認証方式',
-        presenter: '溝口 洸熙, 清水 明宏',
+        presenter: '溝口 洸熙 (KUT)',
         conference: '電子情報通信学会 LOIS研究会',
-        date: '2024-11-15',
+        date: 'Nov. 14, 2024 - Nov. 15, 2024',
         year: 2024,
         place: 'Fukuoka-City, Fukuoka-Pref., Japan.',
-        type: 'Presentation'
+        type: 'OP'
+    },
+    {
+        title: 'A PUF-based Lightweight Authentication and Key Exchange Protocol for IoT with Privacy Protection and Concealed CRP',
+        presenter: 'Koki Mizoguchi (SOKENDAI)',
+        conference: 'Security Summer Summit 2025',
+        date: 'July 7, 2025 - July 9, 2025',
+        year: 2025,
+        place: 'Sapporo-city, Hokkaido, Japan.',
+        type: 'OP'
     }
+
 ]
