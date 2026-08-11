e8c92b97d6032f5bbe5805e72b3b2c3d1c9d9ffc
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2026-08-12 01:03:10 +0900

    update

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -17,7 +17,6 @@ export default function Contact() {
 						style={{ verticalAlign: 'middle', width: 'auto', height: '1em' }} />
 				</p>
 				<h2>Address</h2>
-				<p>Letters and packages should be sent to:</p>
 				<p>
 					〒101-8430<br />
 					東京都 千代田区 一ツ橋2丁目1-2 国立情報学研究所 (14F 大学院生室)
